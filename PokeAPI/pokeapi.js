f34f99bc2ed6f1f480b241cd96c5e43b0bace3af
Eric-2804 <ericdayan13@gmail.com>
2025-05-07 10:25:26 -0500

    Funcionalidad de la Pokedex

---
+++ PokeAPI/pokeapi.js
@@ -0,0 +1,9 @@
+//Buscar  dato del pokemon dependiendo de su numero o nombre.
+
+function buscarPokemon(contenedorNumero){
+    let inputId = `pokemonInput${contenedorNumero}`;
+    let nombrePokemon = document.getElementById(inputId).value.trim().toLowerCase();
+    let urlApi = ``
+    console.log(inputId);
+    
+}
